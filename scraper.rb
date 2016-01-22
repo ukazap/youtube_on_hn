e78c46aeaa2dd729f7b5f22ab07502169e714006
Ukaza Perdana <ukazap@live.com>
2016-01-22 11:18:47 +0700

    puts what the scraper's doing

--- scraper.rb
+++ scraper.rb
@@ -8,13 +8,18 @@ page = agent.get "https://news.ycombinator.com/from?site=youtube.com"
 loop do
   items = page.search(".votelinks")
   items.each do |item|
-    item_json = JSON.parse(open("https://hacker-news.firebaseio.com/v0/item/#{item.at("a")[:id].split("_").last}.json").read)
-    ScraperWiki.save_sqlite(["id"], item_json)
+    data = JSON.parse(open("https://hacker-news.firebaseio.com/v0/item/#{item.at("a")[:id].split("_").last}.json").read)
+    ScraperWiki.save_sqlite(["id"], data)
+    puts "Saving #{data["id"]}"
   end
+
   puts "\n\n"
+
   if next_link = page.link_with(text: "More")
+    puts "Turning page"
     page = next_link.click
   else
+    puts "Done."
     break
   end
 end
