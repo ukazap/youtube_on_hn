95ffd08a2d7b90d6987c4d7cecbe685a1a088562
Ukaza Perdana <ukazap@live.com>
2016-01-22 11:14:35 +0700

    write scraper

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,20 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
+require 'open-uri'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+agent = Mechanize.new
+page = agent.get "https://news.ycombinator.com/from?site=youtube.com"
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+loop do
+  items = page.search(".votelinks")
+  items.each do |item|
+    item_json = JSON.parse(open("https://hacker-news.firebaseio.com/v0/item/#{item.at("a")[:id].split("_").last}.json").read)
+    ScraperWiki.save_sqlite(["id"], item_json)
+  end
+  puts "\n\n"
+  if next_link = page.link_with(text: "More")
+    page = next_link.click
+  else
+    break
+  end
+end
