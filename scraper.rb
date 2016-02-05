9311e3cae5dbe8d37b305512d409f0a060436fd1
Ukaza Perdana <ukazap@live.com>
2016-02-05 07:23:34 +0700

    Error handling for parsing JSON

--- scraper.rb
+++ scraper.rb
@@ -2,31 +2,38 @@ require 'scraperwiki'
 require 'mechanize'
 require 'open-uri'
 
-def scrape_from(site)
+def scrape_from site
   agent = Mechanize.new
   page = agent.get "https://news.ycombinator.com/from?site=#{site}"
 
-  page_number = 1
+  page_number = 0
   loop do
+    puts "Scanning page ##{page_number += 1} for #{site}"
     items = page.search(".votelinks")
     items.each do |item|
-      item_json = JSON.parse(open("https://hacker-news.firebaseio.com/v0/item/#{item.at("a")[:id].split("_").last}.json").read)
-      data = {
-        :id => item_json["id"],
-        :by => item_json["by"],
-        :score => item_json["score"],
-        :time => item_json["time"],
-        :title => item_json["title"],
-        :youtube_url => item_json["url"]
-      }
-      ScraperWiki.save_sqlite([:id, :youtube_url], data) if data[:youtube_url] =~ /(\/watch?)|(youtu.be\/)/
-      puts "Add #{data[:id]}: #{data[:title]}"
+      item_id = item.at("a")[:id].split("_").last
+      begin
+        item_json = JSON.parse(open("https://hacker-news.firebaseio.com/v0/item/#{item_id}.json").read)
+        data = {
+          :id => item_json["id"],
+          :by => item_json["by"],
+          :score => item_json["score"],
+          :time => item_json["time"],
+          :title => item_json["title"],
+          :youtube_url => item_json["url"]
+        }
+        if data[:youtube_url] =~ /(\/watch?)|(youtu.be\/)/
+          ScraperWiki.save_sqlite([:id, :youtube_url], data)
+          puts "Add #{data[:id]}: #{data[:title]}"
+        end
+      rescue
+        puts "Error: #{item_id}"
+        next
+      end
     end
-
-    puts "\n\n"
+    puts "\n"
 
     if next_link = page.link_with(text: "More")
-      puts "Turning to page #{page_number += 1}"
       page = next_link.click
     else
       puts "Done."
